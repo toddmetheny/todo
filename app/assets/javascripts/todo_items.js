397f6292dbafb2dfa48635d24aef702a05fa6cd0
Todd Metheny <todd.metheny@gmail.com>
2014-11-16 19:46:15 -0500

    added inline new item with jQuery

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -3,9 +3,50 @@
 
 
 $(document).ready(function(){
-  $(".checkbox").change(function(){
-    if(this.checked){
-      $('#'+this.id).closest('tr').fadeOut()
-    }
-  });
+  // $(".checkbox").change(function(){
+  //   if(this.checked){
+  //     $('#'+this.id).closest('tr').fadeOut()
+  //   }
+  // });
+	$('tr').on('mouseenter', function(){
+		$(this).css({'background-color': '#66FF33', 'font-weight': 'bold'});
+	}).on('mouseleave', function(){
+		$(this).css({'background-color': '#fff', 'font-weight': 'normal'})
+	});
+
+	$("#the_box").on('click', function(){
+		if(this.checked){
+		  $(this).closest('tr').fadeOut();
+		}
+	})
+
+	var page = "/new.html.erb"
+
+	$(".modal-dreaming").on('click', function(){
+		$(this).dialog({ modal: true })
+	})
+
+	$("#somediv").click(function() {
+  		opendialog("http://localhost:3000/todo_items/new");
 });
+
+	function opendialog(page) {
+	  var $dialog = $('#somediv')
+	  .html('<iframe style="border: 0px; " src="' + page + '" width="100%" height="100%"></iframe>')
+	  .dialog({
+	    title: "Page",
+	    autoOpen: false,
+	    dialogClass: 'dialog_fixed,ui-widget-header',
+	    modal: true,
+	    height: 500,
+	    minWidth: 400,
+	    minHeight: 400,
+	    draggable:true,
+	    /*close: function () { $(this).remove(); },*/
+	    buttons: { "Ok": function () {         $(this).dialog("close"); } }
+	  });
+	  $dialog.dialog('open');
+	}
+
+});
+

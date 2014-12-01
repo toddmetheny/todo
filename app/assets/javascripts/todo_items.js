b5833f5c1a6bd344827ca0dd17b2f248bcd3ff47
Todd Metheny <todd.metheny@gmail.com>
2014-11-30 20:45:00 -0500

    styling and some javascript

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -32,28 +32,16 @@ $(document).ready(function(){
 // 	})
 	$("#somediv").on("click", function(){
 		opendialog("http://localhost:3000/todo_items/edit");
-	})
+	});
 // 	$("#somediv").click(function() {
 //   		opendialog("http://localhost:3000/todo_items/edit");
 // });
+	$("#notice").hide(5000, function(){
+		$(this).remove();
+	});
 
-	function opendialog(page) {
-	  var $dialog = $('#somediv')
-	  .html('<iframe style="border: 0px; " src="' + page + '" width="100%" height="100%"></iframe>')
-	  .dialog({
-	    title: "Page",
-	    autoOpen: false,
-	    dialogClass: 'dialog_fixed,ui-widget-header',
-	    modal: true,
-	    height: 500,
-	    minWidth: 400,
-	    minHeight: 400,
-	    draggable:true,
-	    /*close: function () { $(this).remove(); },*/
-	    buttons: { "Ok": function () {         $(this).dialog("close"); } }
-	  });
-	  $dialog.dialog('open');
-	}
-
+	// $('#myModal').on("click", function(){
+	// 	$(this).modal('show');
+	// });
 });
 

196d019dcac396507b0df0d6b7ce30edb7099817
Todd Metheny <todd.metheny@gmail.com>
2014-12-01 12:04:22 -0500

    create a new list without redirecting to a new page

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -19,48 +19,13 @@ $(document).ready(function(){
 		$(this).css({'background-color': '#fff', 'font-weight': 'normal'})
 	});
 
-	// $("#the_box").on('click', function(){
-	// 	if(this.checked){
-	// 	  $(this).closest('tr').fadeOut();
-	// 	}
-	// })
-
- 	// var page = "/edit.html.erb"
-
-// 	$(".modal-dreaming").on('click', function(){
-// 		$(this).dialog({ modal: true })
-// 	})
-	// $("#somediv").on("click", function(){
-	// 	opendialog("#{edit_list_todo_item_path}");
-	// })
-// 	$("#somediv").click(function() {
-//   		opendialog("http://localhost:3000/todo_items/edit");
-// });
-
-	
-
-	// function opendialog(page) {
-	//   var $dialog = $('#somediv')
-	//   .html('<iframe style="border: 0px; " src="' + page + '" width="100%" height="100%"></iframe>')
-	//   .dialog({
-	//     title: "Page",
-	//     autoOpen: false,
-	//     dialogClass: 'dialog_fixed,ui-widget-header',
-	//     modal: true,
-	//     height: 500,
-	//     minWidth: 400,
-	//     minHeight: 400,
-	//     draggable:true,
-	//     /*close: function () { $(this).remove(); },*/
-	//     buttons: { "Ok": function () {         $(this).dialog("close"); } }
-	//   });
-	//   $dialog.dialog('open');
-	// }
 
 	$("#notice").fadeOut(4000, function(){
 		$(this).remove();
 	});
 
+
+	// $('#new_link').hide().after('<%= j render("form") %>');
 
 	// $("#new_todo_item > div.actions > input").on("click", function(){
 	// 	console.log($(this))
